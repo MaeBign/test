d70d93953b035e4c84b4c216c17997597dd09d8c
MaeBign <mael.bgr1@gmail.com>
2024-06-22 21:34:40 +0200

    Delete blog directory

--- blog/footer.php
+++
@@ -1,3 +0,0 @@
-<div id="footer">
-	<p class="footer-text">copyright me</p>
-</div>
--- blog/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-require_once(__DIR__ . '/functions.php');
-session_unset();
-session_destroy();
-redirectTo('index.php');
-?>
